f41584f1a13b761ab9bdf480b808e8288c795c88
kasperisager <kasperisager@gmail.com>
2014-03-25 22:10:30 -0400

    Improvements to `fish_prompt`

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,3 +1,32 @@
+################################################################################
+# Git                                                                          #
+################################################################################
+
+set -g __fish_git_prompt_show_informative_status 1
+set -g __fish_git_prompt_hide_untrackedfiles 1
+set -g __fish_git_prompt_show_upstream "informative"
+
+set -g __fish_git_prompt_char_upstream_ahead "↑ "
+set -g __fish_git_prompt_char_upstream_behind "↓ "
+set -g __fish_git_prompt_char_upstream_prefix "|"
+
+set -g __fish_git_prompt_char_stagedstate "●"
+set -g __fish_git_prompt_char_dirtystate "✚"
+set -g __fish_git_prompt_char_untrackedfiles "…"
+set -g __fish_git_prompt_char_conflictedstate "✖"
+set -g __fish_git_prompt_char_cleanstate "✔"
+
+set -g __fish_git_prompt_color_branch blue bold
+set -g __fish_git_prompt_color_dirtystate blue
+set -g __fish_git_prompt_color_stagedstate yellow
+set -g __fish_git_prompt_color_invalidstate red
+set -g __fish_git_prompt_color_untrackedfiles $fish_color_normal
+set -g __fish_git_prompt_color_cleanstate green bold
+
+################################################################################
+# Prompt                                                                       #r
+################################################################################
+
 function fish_prompt --description "Write out the prompt"
 
     set -l last_status $status
@@ -6,17 +35,17 @@ function fish_prompt --description "Write out the prompt"
         set -g __fish_prompt_normal (set_color normal)
     end
 
-    # PWD
     set_color $fish_color_cwd
     echo -n (prompt_pwd)
     set_color normal
 
-    printf "%s " (__fish_git_prompt)
+    echo -n (__fish_git_prompt) \n
 
     if not test $last_status -eq 0
-    set_color $fish_color_error
+        set_color $fish_color_error
     end
 
-    echo -n '$ '
+    echo -n "\$ "
+    set_color normal
 
 end
