e3a77de6303ce0cac2d53b247aae976ff4aaf4cb
kasperisager <kasperisager@gmail.com>
2014-04-02 13:16:09 -0400

    Atom FTW! :boom:

--- script/symlink.sh
+++ script/symlink.sh
@@ -14,6 +14,14 @@ dotfile .vimrc
 # Create required Fish directories
 mkdir -p $HOME/.config/fish
 
+# Atom configuration
+ln -sf $HOME/.dotfiles/atom/config.cson $HOME/.atom 2> /dev/null
+ln -sf $HOME/.dotfiles/atom/keymap.cson $HOME/.atom 2> /dev/null
+ln -sf $HOME/.dotfiles/atom/packages.cson $HOME/.atom 2> /dev/null
+ln -sf $HOME/.dotfiles/atom/snippets.cson $HOME/.atom 2> /dev/null
+ln -sf $HOME/.dotfiles/atom/init.coffee $HOME/.atom 2> /dev/null
+ln -sf $HOME/.dotfiles/atom/styles.less $HOME/.atom 2> /dev/null
+
 # Fish config and functions directory
 ln -sf $HOME/.dotfiles/fish/config.fish $HOME/.config/fish 2> /dev/null
 ln -sf $HOME/.dotfiles/fish/functions $HOME/.config/fish/functions 2> /dev/null
