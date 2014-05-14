dd6df1190200aae04a83232db7a663818cfc6142
kasperisager <kasperisager@gmail.com>
2014-05-14 10:46:16 -0400

    Adjust Atom path

--- fish/config.fish
+++ fish/config.fish
@@ -3,3 +3,6 @@
 
 # Set GOPATH and prepend the `bin` dir to PATH
 set -x GOPATH "$HOME/Development/go"; set PATH $GOPATH/bin $PATH
+
+# Set correct Atom application path
+set -x ATOM_PATH "$HOME/Applications"
