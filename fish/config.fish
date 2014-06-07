90b6217e9a6ef2b82c7682b31361b667466955c1
kasperisager <kasperisager@gmail.com>
2014-06-06 21:10:55 -0400

    Report terminal type

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,9 @@
 # Aliases shared between fish and bash
 . ~/.aliases
 
+# Report terminal type
+set -x TERM "xterm-256color"
+
 # Set GOPATH and prepend the `bin` dir to PATH
 set -x GOPATH "$HOME/Development/go"; set PATH $GOPATH/bin $PATH
 
