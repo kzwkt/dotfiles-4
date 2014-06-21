88d76f1d2a1f15fcfc2bd9579de25439d742f42a
kasperisager <kasperisager@gmail.com>
2014-06-20 22:03:02 -0400

    Fix Atom config symlinking

--- script/symlink.sh
+++ script/symlink.sh
@@ -11,26 +11,29 @@ dotfile .gitconfig
 dotfile .gitignore
 dotfile .vimrc
 
+# Create required Atom directories
+mkdir -p $HOME/.atom
+
+# Atom configuration
+ln -sf $HOME/.dotfiles/atom/config.cson $HOME/.atom/ 2> /dev/null
+ln -sf $HOME/.dotfiles/atom/keymap.cson $HOME/.atom/ 2> /dev/null
+ln -sf $HOME/.dotfiles/atom/packages.cson $HOME/.atom/ 2> /dev/null
+ln -sf $HOME/.dotfiles/atom/snippets.cson $HOME/.atom/ 2> /dev/null
+ln -sf $HOME/.dotfiles/atom/init.coffee $HOME/.atom/ 2> /dev/null
+ln -sf $HOME/.dotfiles/atom/styles.less $HOME/.atom/ 2> /dev/null
+
 # Create required Fish directories
 mkdir -p $HOME/.config/fish
 
-# Atom configuration
-ln -sf $HOME/.dotfiles/atom/config.cson $HOME/.atom 2> /dev/null
-ln -sf $HOME/.dotfiles/atom/keymap.cson $HOME/.atom 2> /dev/null
-ln -sf $HOME/.dotfiles/atom/packages.cson $HOME/.atom 2> /dev/null
-ln -sf $HOME/.dotfiles/atom/snippets.cson $HOME/.atom 2> /dev/null
-ln -sf $HOME/.dotfiles/atom/init.coffee $HOME/.atom 2> /dev/null
-ln -sf $HOME/.dotfiles/atom/styles.less $HOME/.atom 2> /dev/null
-
 # Fish config and functions directory
-ln -sf $HOME/.dotfiles/fish/config.fish $HOME/.config/fish 2> /dev/null
-ln -sf $HOME/.dotfiles/fish/functions $HOME/.config/fish 2> /dev/null
+ln -sf $HOME/.dotfiles/fish/config.fish $HOME/.config/fish/ 2> /dev/null
+ln -sf $HOME/.dotfiles/fish/functions $HOME/.config/fish/ 2> /dev/null
 
 # Create required Vim directories
 mkdir -p $HOME/.vim/{backups,swaps,undo}
 
 # Vim color schemes and bundles
-ln -sf $HOME/.dotfiles/vim/colors $HOME/.vim 2> /dev/null
-ln -sf $HOME/.dotfiles/vim/bundle $HOME/.vim 2> /dev/null
+ln -sf $HOME/.dotfiles/vim/colors $HOME/.vim/ 2> /dev/null
+ln -sf $HOME/.dotfiles/vim/bundle $HOME/.vim/ 2> /dev/null
 
 unset dotfile
