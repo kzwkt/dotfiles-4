122dbf988b9d0864297c73ef36691631eb0c8463
kasperisager <kasperisager@gmail.com>
2014-06-20 21:56:06 -0400

    Fix pathogen.vim download

--- vim/pathogen.sh
+++ vim/pathogen.sh
@@ -3,4 +3,4 @@
 # Install Pathogen
 mkdir -p ~/.vim/autoload; \
 curl -Sso ~/.vim/autoload/pathogen.vim \
-    https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+    https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim
