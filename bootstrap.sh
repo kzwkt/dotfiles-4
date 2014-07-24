d03d48b617b4b32e85ba77e2ee4ef9f957307fed
Kasper Isager <kasperisager@gmail.com>
2014-07-23 22:49:18 -0400

    Less hacky Bork wrapper.

---
+++ bootstrap.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+destination $HOME/.dotfiles
+
+# Update Bork
+ok github mattly/bork
+
+# All UNIXes
+include configs.sh
+include etc.sh
+include vim.sh
+include fish.sh
+include atom.sh
+
+# OS specfic
+case $OSTYPE in
+  darwin*)
+    include brew.sh
+    include cask.sh
+    include osx.sh
+    ;;
+esac
