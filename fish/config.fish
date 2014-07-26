4b020135e26e276f3946f34b6bc9c25122cb1ca7
Kasper Isager <kasperisager@gmail.com>
2014-07-26 14:17:08 -0400

    Remove ATOM_PATH.

--- fish/config.fish
+++ fish/config.fish
@@ -7,8 +7,5 @@ set -x TERM "xterm-256color"
 # Set GOPATH and prepend the `bin` dir to PATH
 set -x GOPATH "$HOME/Development/go"; set PATH $GOPATH/bin $PATH
 
-# Set correct Atom application path
-set -x ATOM_PATH "$HOME/Applications"
-
 # Set Docker host
 set -x DOCKER_HOST "tcp://localhost:2375"
