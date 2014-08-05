4dff1ae9180f9206267623956c632cc99fec2b5f
Kasper Isager <kasperisager@gmail.com>
2014-08-05 19:43:38 -0400

    More npm packages.

--- npm.sh
+++ npm.sh
@@ -1,2 +1,20 @@
+# npm-ception
+ok npm npm
+
+# Env management
 ok npm n
+
+# System utils
 ok npm trash
+
+# Package managers
+ok npm bower
+ok npm jspm
+
+# Task runners
+ok npm broccoli-cli
+ok npm gulp
+ok npm grunt-cli
+
+# Misc.
+ok npm divshot-cli
