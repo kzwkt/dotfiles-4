bb995447b9bb9ce995dcda87ad67a382353c7884
Kasper Isager <kasperisager@gmail.com>
2015-02-15 22:09:51 +0100

    Set Java classpath.

--- fish/config.fish
+++ fish/config.fish
@@ -7,6 +7,9 @@ set -x TERM "xterm-256color"
 # Set GOPATH and prepend the `bin` dir to PATH
 set -x GOPATH "$HOME/Development/go"; set PATH $GOPATH/bin $PATH
 
+# Set Java classpath
+set -x CLASSPATH ".:$HOME/Development/java/lib/*";
+
 # Set correct Atom application path
 set -x ATOM_PATH "$HOME/Applications"
 
