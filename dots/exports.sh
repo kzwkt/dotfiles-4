65c7e7d771e224b37ef8e17dd60db5b4d02a33d3
Kasper Isager <kasperisager@gmail.com>
2015-10-21 15:53:58 +0200

    Move some stuff around

---
+++ dots/exports.sh
@@ -0,0 +1,9 @@
+# Composer (https://github.com/composer/composer)
+export PATH="$PATH:$HOME/.composer/vendor/bin"
+
+# Java (https://www.java.com/en/)
+export CLASSPATH=".:$HOME/Development/java/lib/*"
+
+# Go (https://golang.org/)
+export GOPATH="$HOME/Development/go"
+export PATH="$PATH:$GOPATH/bin"
