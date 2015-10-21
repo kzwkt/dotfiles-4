8ade57caec4c50cb43f2bf80d7f8a91d90785be6
Kasper Isager <kasperisager@gmail.com>
2015-10-21 14:37:43 +0200

    Include only needed global gitignores

--- git.sh
+++ git.sh
@@ -2,4 +2,9 @@ destination $HOME/.dotfiles
 
 ok github github/gitignore
 
-cat gitignore/Global/*.gitignore > $HOME/.gitignore
+cat gitignore/Global/{\
+Linux,\
+OSX,\
+Vim,\
+Windows\
+}.gitignore > $HOME/.gitignore
