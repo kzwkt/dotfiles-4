fe540a597b092516e5058d3ff2b1300041013ea0
Kasper Isager <kasperisager@gmail.com>
2016-10-25 09:16:31 +0200

    Set Mono path

--- dots/exports.sh
+++ dots/exports.sh
@@ -8,5 +8,8 @@ export CLASSPATH=".:$HOME/Development/java/lib/*"
 export GOPATH="$HOME/Development/go"
 export PATH="$PATH:$GOPATH/bin"
 
+# Mono (http://www.mono-project.com/)
+export MONO_PATH="/usr/local/lib/mono_assemblies"
+
 # Report terminal type
 export TERM="xterm-256color"
