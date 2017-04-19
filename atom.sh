2a904dbb856eba77eaadffa1974cba14c00dbf2f
Kasper Isager <kasperisager@gmail.com>
2017-04-19 09:22:36 +0200

    Tweak Atom packages

--- atom.sh
+++ atom.sh
@@ -7,16 +7,12 @@ for file in $HOME/.dotfiles/atom/*; do
 done
 unset file
 
-# Master of ceremonies
-ok apm package-sync
-
 # General
 ok apm advanced-open-file
 ok apm atom-ternjs
 ok apm docblockr
 ok apm editorconfig
 ok apm file-icons
-ok apm pigments
 ok apm sort-lines
 ok apm tabs-to-spaces
 ok apm vim-mode
@@ -27,27 +23,30 @@ ok apm merge-conflicts
 
 # Linters
 ok apm linter
-ok apm linter-coffeelint
-ok apm linter-cpplint
+ok apm linter-clang
 ok apm linter-javac
 ok apm linter-js-yaml
 ok apm linter-jsonlint
 ok apm linter-php
-ok apm linter-puppet
-ok apm linter-xmllint
+ok apm linter-scalac
 ok apm linter-xo
 
+# Formatters
+ok apm formatter
+ok apm formatter-gofmt
+
 # Languages
+ok apm language-cmake
 ok apm language-docker
+ok apm language-dot
 ok apm language-dotenv
-ok apm language-eco
-ok apm language-ejs
-ok apm language-fish-shell
 ok apm language-gitignore
-ok apm language-handlebars
+ok apm language-haskell
 ok apm language-latex
-ok apm language-puppet
-ok apm language-smarty
+ok apm language-protobuf
+ok apm language-scala
+ok apm language-systemd
+ok apm language-terraform
 ok apm language-viml
 
 # Autocompletion
@@ -55,12 +54,3 @@ ok apm autocomplete-modules
 ok apm autocomplete-paths
 ok apm autocomplete-php
 ok apm autocomplete-snippets
-
-# Minimap
-ok apm minimap
-ok apm minimap-find-and-replace
-ok apm minimap-git-diff
-
-# Ionide
-ok apm ionide-fsharp
-ok apm ionide-fsi
