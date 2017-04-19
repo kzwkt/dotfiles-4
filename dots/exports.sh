5f363fa44eca1e3253a3f567353506d29edd01f2
Kasper Isager <kasperisager@gmail.com>
2017-04-19 09:25:57 +0200

    Add some more exports

--- dots/exports.sh
+++ dots/exports.sh
@@ -3,10 +3,14 @@ export PATH="$PATH:$HOME/.composer/vendor/bin"
 
 # Java (https://www.java.com/en/)
 export CLASSPATH=".:$HOME/Development/java/lib/*"
+export JAVA_HOME="$(/usr/libexec/java_home)"
 
 # Go (https://golang.org/)
 export GOPATH="$HOME/Development/go"
 export PATH="$PATH:$GOPATH/bin"
+
+# Yarn (https://yarnpkg.com/)
+export PATH="$PATH:$HOME/.config/yarn/global/node_modules/.bin"
 
 # Mono (http://www.mono-project.com/)
 export MONO_PATH="/usr/local/lib/mono_assemblies"
