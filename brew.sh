de33fb9e79b2e57daef6dd5c2c9552ffcc6a3131
Kasper Isager <kasperisager@gmail.com>
2017-10-09 20:07:04 +0200

    Update brews

--- brew.sh
+++ brew.sh
@@ -2,26 +2,22 @@
 ok brew
 
 # Taps
-ok brew-tap aspnet/dnx
 ok brew-tap homebrew/dupes
 ok brew-tap getantibody/homebrew-antibody
 
 # Install formulae
-ok brew ack
 ok brew antibody
 ok brew docker
 ok brew docker-compose
 ok brew docker-machine
 ok brew docker-swarm
-ok brew dnvm
+ok brew fd
 ok brew git
 ok brew go
-ok brew httpie
 ok brew hub
-ok brew mono
 ok brew node
 ok brew python
-ok brew tmux
+ok brew the_silver_searcher
 ok brew vim
 ok brew zsh
 
